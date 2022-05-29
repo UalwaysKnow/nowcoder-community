203907e79fac9d07bb62ce717da0d72ce11e9fdc
yiweizhang526 <yiweizhang526@gmail.com>
2022-05-29 22:23:45 +0800

    login function

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketMapper {
+    // 这里不用xml配置文件的方式，用注解的方式来练习
+    @Insert({
+            "insert into login_ticket(user_id,ticket,status,expired) ",
+            "values(#{userId},#{ticket},#{status},#{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    @Select({
+            "select id,user_id,ticket,status,expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({ // 这里只是演示脚本形式的写法
+            "<script>",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\"> ",
+            "and 1=1 ",
+            "</if>",
+            "</script>"
+    })
+    int updateStatus(String ticket, int status);
+
+
+
+}
