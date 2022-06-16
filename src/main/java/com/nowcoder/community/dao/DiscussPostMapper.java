24d45c6870b6367e46ac25497ae757010f5187f3
yiweizhang526 <yiweizhang526@gmail.com>
2022-06-16 20:08:45 +0800

    spring security three functions

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -21,4 +21,10 @@ public interface DiscussPostMapper {
 
     int updateCommentCount(int id, int commentCount);
 
+    int updateType(int id, int type);
+
+    int updateStatus(int id, int status);
+
+    int updateScore(int id, double score);
+
 }
